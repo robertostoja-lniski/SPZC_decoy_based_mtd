02b61f1148a8d63ca130c694d568d16953d98336
robertostoja-lniski <robert.lniski@gmail.com>
2021-06-13 18:17:53 +0200

    Reset counter update

--- test/reset_counter.sh
+++ test/reset_counter.sh
@@ -3,11 +3,13 @@ for num_nodes in {1..10}
 do
 
   SECONDS=0
-  gcloud container clusters resize spzc-reset-ip --num-nodes $num_nodes --zone us-central1-c -q > /dev/null
-  echo Adding $num_nodes nodes took: $SECONDS seconds
+  new_nodes_num=$(( $num_nodes + $num_nodes))
+#  echo $new_nodes_num
+  gcloud container clusters resize spzc-reset-ip --num-nodes $new_nodes_num --zone us-central1-c -q > /dev/null
+  echo Adding $new_nodes_num nodes took: $SECONDS seconds
 
   SECONDS=0
-  gcloud container clusters resize spzc-reset-ip --num-nodes $num_nodes --zone us-central1-c -q > /dev/null
+#  gcloud container clusters resize spzc-reset-ip --num-nodes $num_nodes --zone us-central1-c -q > /dev/null
   echo Removing $num_nodes old nodes nodes took: $SECONDS seconds
 
 done
